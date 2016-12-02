39cd21be6807f6ba448fdfa6fecb67b9986ecee8
payeboland <payeboland2014@gmail.com>
2016-12-02 15:42:51 +0330

    Add files via upload

--- maker.py
+++ maker.py
@@ -36,7 +36,7 @@ def maker1_f(name,phone,desc,email,website):
     namedisp=arabic_reshaper.reshape(name)
     namedisp = get_display(namedisp)
 
-    w, h = draw.textsize(back,font)
+    w, h = draw.textsize(namedisp,font)
     draw.text(( (img.width -w)/2 , img.height/2 - 40 ),namedisp,(255,255,255),font=font)
 
 
@@ -45,7 +45,9 @@ def maker1_f(name,phone,desc,email,website):
     #desc = desc.decode('utf-8')
     descdisp=arabic_reshaper.reshape(desc)
     descdisp = get_display(descdisp)
-    draw.text((img.width / 2 -14*len(desc)+2 , img.height/2 +200),descdisp,(255,255,255),font=font)
+
+    w, h = draw.textsize(descdisp,font)
+    draw.text(( (img.width -w)/2 , img.height/2 +200),descdisp,(255,255,255),font=font)
 
 
 
@@ -98,45 +100,5 @@ def maker1_b(back,backsub):
     return photoadd
 
 
-def maker1(name,phone,desc):
-    img = Image.open("bg1.png")
-    draw = ImageDraw.Draw(img)
-    # font = ImageFont.truetype(<font-file>, <font-size>)
-    font = ImageFont.truetype("titr.ttf", 16, encoding='unic')
-    # draw.text((x, y),"Sample Text",(r,g,b))
-
-    draw.text_alignment = 'right';
-    draw.text_antialias = True
-    draw.text_encoding = 'utf-8'
-    
-    font = ImageFont.truetype("titr.ttf", 17, encoding='unic')
-    #phone = phone.decode('utf-8')
-    disp=arabic_reshaper.reshape(phone)
-    disp = get_display(disp)
-    draw.text((5, 5),disp,(255,255,255),font=font)
-
-    font = ImageFont.truetype("titr.ttf", 45, encoding='unic')
-    name=' '+name
-    #name = name.decode('utf-8')
-    namedisp=arabic_reshaper.reshape(name)
-    namedisp = get_display(namedisp)
-    draw.text((img.width / 2 -12*len(name) , img.height/2 - 100),namedisp,(0,0,255),font=font)
-
-
-    font = ImageFont.truetype("Yekan.ttf", 30, encoding='unic')
-    desc=' '+desc
-    #desc = desc.decode('utf-8')
-    descdisp=arabic_reshaper.reshape(desc)
-    descdisp = get_display(descdisp)
-    draw.text((img.width / 2 -8*len(desc) , img.height/2 - 10),descdisp,(0,0,255),font=font)
-
-
-
-
-
-    photoadd=str(randint(111,9876543210))+".png"
-    img.save(photoadd)
-
-    return photoadd
 
 
